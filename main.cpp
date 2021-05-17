7b47e94e131cd55619ddbb880b7e37a97fe93079
Isaac Ayala <darksonic007@gmail.com>
2021-05-16 21:50:43 -0600

    Proyecto

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+//Proyecto Domótica aplicada a una casa de habitacion controlada desde el celular
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
